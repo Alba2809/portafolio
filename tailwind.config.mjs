457b9da67ad2534aabd875e3406f60b412de434a
Alba2809 <197o00679@itsx.edu.mx>
2024-08-24 19:56:41 -0600

    Avance de los proyectos, about me, imagenes, etc.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,21 @@
+import { transform } from 'typescript';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				'infinite-scroll': 'infinite-scroll 25s linear infinite'
+			},
+			keyframes: {
+				'infinite-scroll': {
+					from: { transform: 'translateX(0)' },
+					to: { transform: 'translateX(-100%)' }
+				}
+			}
+		},
 	},
 	plugins: [],
 }
